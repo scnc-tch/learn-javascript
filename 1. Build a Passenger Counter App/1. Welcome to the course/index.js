b9ec7e21b6263656cdf5a640b8488a101ffb2a0a
Michael Correa <98114967+scnc-tch@users.noreply.github.com>
2022-03-07 21:24:12 -0500

    03/07/22
    Work for the day..to be cont

--- 1. Build a Passenger Counter App/1. Welcome to the course/index.js
+++ 1. Build a Passenger Counter App/1. Welcome to the course/index.js
@@ -1,1 +1,15 @@
-// javascript
+// document.getElementById('').innerText = ''
+
+let count = 0;
+let elementCount = document.getElementById('count');
+
+function incrementCount(){
+    count = count + 1;
+    elementCount.innerText = count
+}
+
+function save() {
+    console.log(count);
+}
+
+// 46:12 / 7:44:19
